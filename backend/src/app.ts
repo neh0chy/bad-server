256573eb5a2c483f749ad2de4cfcd578e3d85331
Artem Aisin <artem.aisin@yandex.ru>
2025-01-15 15:14:18 +0300

    feat: implemented rate limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,18 +9,21 @@ import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
+import rateLimit from 'express-rate-limit'
 
 const { PORT = 3000, ORIGIN_ALLOW = 'http://localhost:5173' } = process.env
 const app = express()
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+})
 
 app.use(cookieParser())
-
-// app.use(cors())
 app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }))
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(serveStatic(path.join(__dirname, 'public')))
-
+app.use(limiter)
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
