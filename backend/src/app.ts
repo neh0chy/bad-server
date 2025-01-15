01864305915a81fb8d871c2c1cc23a4d36dbe091
Artem Aisin <artem.aisin@yandex.ru>
2025-01-15 19:00:01 +0300

    fix: prevent mongo injection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 import rateLimit from 'express-rate-limit'
-import ExpressMongoSanitize from 'express-mongo-sanitize'
+import mongoSanitize from 'express-mongo-sanitize'
 
 const { PORT = 3000, ORIGIN_ALLOW = 'http://localhost:5173' } = process.env
 const app = express()
@@ -26,7 +26,7 @@ app.use(serveStatic(path.join(__dirname, 'public')))
 app.use(limiter)
 app.use(urlencoded({ extended: true }))
 app.use(json())
-app.use(ExpressMongoSanitize())
+app.use(mongoSanitize())
 app.options('*', cors())
 app.use(routes)
 app.use(errors())
