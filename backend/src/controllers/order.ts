a0fed52a020f4edc684fec5500e461436181c6f1
Artem Aisin <artem.aisin@yandex.ru>
2025-01-15 16:19:24 +0300

    fix: sanitize comment

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -5,7 +5,8 @@ import NotFoundError from '../errors/not-found-error'
 import Order, { IOrder } from '../models/order'
 import Product, { IProduct } from '../models/product'
 import User from '../models/user'
-import escapeRegExp from 'utils/escapeRegExp'
+import escapeRegExp from '../utils/escapeRegExp'
+import { escape } from 'validator'
 
 // eslint-disable-next-line max-len
 // GET /orders?page=2&limit=5&sort=totalAmount&order=desc&orderDateFrom=2024-07-01&orderDateTo=2024-08-01&status=delivering&totalAmountFrom=100&totalAmountTo=1000&search=%2B1
@@ -311,13 +312,15 @@ export const createOrder = async (
             return next(new BadRequestError('Неверная сумма заказа'))
         }
 
+        const sanitizedComment = escape(comment)
+
         const newOrder = new Order({
             totalAmount: total,
             products: items,
             payment,
             phone,
             email,
-            comment,
+            comment: sanitizedComment,
             customer: userId,
             deliveryAddress: address,
         })
