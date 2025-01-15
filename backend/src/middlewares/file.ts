c8e50cee495ca6ad52cb27ad9c9c8e8772a9a026
Artem Aisin <artem.aisin@yandex.ru>
2025-01-15 17:03:57 +0300

    fix: change filenames for paths

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -1,6 +1,7 @@
 import { Request, Express } from 'express'
+import { faker } from '@faker-js/faker'
 import multer, { FileFilterCallback } from 'multer'
-import { join } from 'path'
+import path, { join } from 'path'
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
@@ -24,10 +25,10 @@ const storage = multer.diskStorage({
 
     filename: (
         _req: Request,
-        file: Express.Multer.File,
+        _file: Express.Multer.File,
         cb: FileNameCallback
     ) => {
-        cb(null, file.originalname)
+        cb(null, `${faker.string.uuid()}${path.extname(_file.originalname)}`)
     },
 })
 
